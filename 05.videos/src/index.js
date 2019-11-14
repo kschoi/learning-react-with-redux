5bb030ebfe3d29b0a7087ebc425aafeaad248c30
kyungsun choi <kschoi0308@gmail.com>
2019-11-14 13:57:11 +0900

    browserroter 추가

--- 05.videos/src/index.js
+++ 05.videos/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter basename="/LearningReactWithRedux">
+    <App />
+  </BrowserRouter>,
   document.querySelector('#root')
 )
